f73f02cee3e7ff58bd58c0142965f250f6ae2aa3
= <=>
2020-08-30 08:58:57 +0300

    detail activity created and card view library added

---
+++ app/src/main/java/com/example/kotlinretrofit/UI/Activity/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinretrofit.UI.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinretrofit.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
